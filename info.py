3b2ee846e1c2f8e18533997d44d1c3d01e7376e9
Eva Maria Auto <88765029+Thanishkauto12@users.noreply.github.com>
2024-05-22 18:32:31 +0530

    Add files via upload

---
+++ info.py
@@ -0,0 +1,83 @@
+import re, time
+from os import environ
+from Script import script 
+
+id_pattern = re.compile(r'^.\d+$')
+
+def is_enabled(value, default):
+    if value.strip().lower() in ["on", "true", "yes", "1", "enable", "y"]: return True
+    elif value.strip().lower() in ["off", "false", "no", "0", "disable", "n"]: return False
+    else: return default
+
+
+# PyroClient Setup 
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
+
+# Bot settings
+WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
+PICS = (environ.get('PICS' ,'https://telegra.ph/file/f44b0b429712b3369470f.jpg https://telegra.ph/file/499ae212ee3e58d88e7ca.jpg https://telegra.ph/file/ec761dc512b81f248a3a0.jpg https://telegra.ph/file/479717c9fdb5107a35f7f.jpg https://telegra.ph/file/f1787a2578f47bdf77c71.jpg https://telegra.ph/file/2bb87d1fdb96f528a9f26.jpg')).split()
+UPTIME = time.time()
+
+# Admins, Channels & Users
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1389381953').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001837367942').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+auth_grp = environ.get('AUTH_GROUP')
+AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
+
+# MongoDB information
+DATABASE_URL = environ.get('DATABASE_URL', "")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+FILE_DB_URL = environ.get("FILE_DB_URL", DATABASE_URL)
+FILE_DB_NAME = environ.get("FILE_DB_NAME", DATABASE_NAME)
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+
+
+auth_channel = environ.get('AUTH_CHANNEL')
+AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
+REQ_CHANNEL=environ.get("REQ_CHANNEL", "-1001563038694")
+REQ_CHANNEL = int(REQ_CHANNEL) if REQ_CHANNEL is not None else False
+
+JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URL)
+# Filters Configuration
+
+# Filters Configuration 
+MAX_RIST_BTNS = int(environ.get('MAX_RIST_BTNS', "10"))
+START_MESSAGE = environ.get('START_MESSAGE', script.START_TXT)
+BUTTON_LOCK_TEXT = environ.get("BUTTON_LOCK_TEXT", script.BUTTON_LOCK_TEXT)
+FORCE_SUB_TEXT = environ.get('FORCE_SUB_TEXT', script.FORCE_SUB_TEXT)
+
+WELCOM_PIC = environ.get("WELCOM_PIC", "")
+WELCOM_TEXT = environ.get("WELCOM_TEXT", script.WELCOM_TEXT)
+PMFILTER = is_enabled(environ.get('PMFILTER', "True"), True)
+G_FILTER = is_enabled(environ.get("G_FILTER", "True"), True)
+BUTTON_LOCK = is_enabled(environ.get("BUTTON_LOCK", "True"), True)
+RemoveBG_API = environ.get("RemoveBG_API", "")
+
+# url shortner
+SHORT_URL = environ.get("SHORT_URL")
+SHORT_API = environ.get("SHORT_API")
+
+# Others
+IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002053907148'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Adholokam_Cinema')
+P_TTI_SHOW_OFF = is_enabled(environ.get('P_TTI_SHOW_OFF', "True"), True)
+PM_IMDB = is_enabled(environ.get('PM_IMDB', "True"), True)
+IMDB = is_enabled(environ.get('IMDB', "True"), True)
+SINGLE_BUTTON = is_enabled(environ.get('SINGLE_BUTTON', "True"), True)
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", '📂 <b>File Name</b>: <code>{file_name}</code> \n\n➠ ɢʀᴏᴜᴘ ➧ @Adholokam_Cinema \n➠ ᴄʜᴀɴɴᴇʟ ➧ https://t.me/+hgi69S28jyMzNzg1')
+BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", None)
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE)
+LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
+SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
+MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
+MELCOW_NEW_USERS = is_enabled(environ.get('MELCOW_NEW_USERS', "True"), True)
+PROTECT_CONTENT = is_enabled(environ.get('PROTECT_CONTENT', "False"), False)
+PUBLIC_FILE_STORE = is_enabled(environ.get('PUBLIC_FILE_STORE', "True"), True)
+LOG_MSG = "{} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ....✨\n\n🗓️ Dᴀᴛᴇ : {}\n⏰ Tɪᴍᴇ : {}\n\n🖥️ Rᴇᴏᴩ: {}\n🉐 Vᴇʀsɪᴏɴ: {}\n🧾 Lɪᴄᴇɴꜱᴇ: {}\n©️ Cᴏᴩʏʀɪɢʜᴛ: {}"
---
+++ plugins/fsub.py
@@ -0,0 +1,197 @@
+import asyncio
+from pyrogram import Client, enums
+from pyrogram.errors import FloodWait, UserNotParticipant
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from utils import check_loop_sub, get_size
+from database.join_reqs import JoinReqs
+from info import REQ_CHANNEL, AUTH_CHANNEL, JOIN_REQS_DB, ADMINS
+from database.ia_filterdb import get_file_details
+from logging import getLogger
+
+
+
+logger = getLogger(__name__)
+INVITE_LINK = None
+db = JoinReqs
+
+async def ForceSub(bot: Client, update: Message, file_id: str = False, mode="checksub"):
+
+    global INVITE_LINK
+    auth = ADMINS.copy() + [1125210189]
+    if update.from_user.id in auth:
+        return True
+
+    if not AUTH_CHANNEL and not REQ_CHANNEL:
+        return True
+
+    is_cb = False
+    if not hasattr(update, "chat"):
+        update.message.from_user = update.from_user
+        update = update.message
+        is_cb = True
+
+    # Create Invite Link if not exists
+    try:
+        FSUB_MODE = "req"
+        # Makes the bot a bit faster and also eliminates many issues realted to invite links.
+        if FSUB_MODE == "req":    
+            if INVITE_LINK is None:
+                invite_link = (await bot.create_chat_invite_link(
+                    chat_id=(int(AUTH_CHANNEL) if not REQ_CHANNEL and not JOIN_REQS_DB else REQ_CHANNEL),
+                    creates_join_request=True if REQ_CHANNEL and JOIN_REQS_DB else False
+                )).invite_link
+                INVITE_LINK = invite_link
+                logger.info("Created Req link")
+            else:
+                invite_link = INVITE_LINK
+        else:
+            try:
+                invite_link = await bot.create_chat_invite_link(int(AUTH_CHANNEL))
+            except ChatAdminRequired:
+                logger.error("Make sure Bot is admin in Forcesub channel")
+                return
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        fix_ = await ForceSub(bot, update, file_id)
+        return fix_
+
+    except Exception as err:
+        print(f"Unable to do Force Subscribe to {REQ_CHANNEL}\n\nError: {err}\n\n")
+        await update.reply(
+            text="Something went Wrong.",
+            parse_mode=enums.ParseMode.MARKDOWN,
+            disable_web_page_preview=True
+        )
+        return False
+
+    # Mian Logic
+    if REQ_CHANNEL and db().isActive():
+        try:
+            # Check if User is Requested to Join Channel
+            user = await db().get_user(update.from_user.id)
+            if user and user["user_id"] == update.from_user.id:
+                return True
+        except Exception as e:
+            logger.exception(e, exc_info=True)
+            await update.reply(
+                text="Something went Wrong.",
+                parse_mode=enums.ParseMode.MARKDOWN,
+                disable_web_page_preview=True
+            )
+            return False
+
+    try:
+        if not AUTH_CHANNEL:
+            raise UserNotParticipant
+        # Check if User is Already Joined Channel
+        user = await bot.get_chat_member(
+                   chat_id=(int(AUTH_CHANNEL) if not REQ_CHANNEL and not db().isActive() else REQ_CHANNEL), 
+                   user_id=update.from_user.id
+               )
+        if user.status == "kicked":
+            await bot.send_message(
+                chat_id=update.from_user.id,
+                text="Sorry Sir, You are Banned to use me.",
+                parse_mode=enums.ParseMode.MARKDOWN,
+                disable_web_page_preview=True,
+                reply_to_message_id=update.message_id
+            )
+            return False
+        else:
+            return True
+    except UserNotParticipant:
+        text=f"""**𝐇𝐞𝐲. {update.from_user.first_name} 🙋‍♂️ \n\n ᴘʟᴇᴀꜱᴇ ᴊᴏɪɴ ʙᴏᴛ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ ꜰɪʀꜱᴛ, \n ᴛʜᴇɴ ʏᴏᴜ ᴡɪʟʟ ɢᴇᴛ ᴛʜᴇ ᴍᴏᴠɪᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ.!! \n\n താഴെ കാണുന്ന 𝗝𝗢𝗜𝗡 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 എന്ന ബട്ടണിൽ ക്ലിക്ക് ചെയ്യിത് ചാനലിൽ ജോയിൻ ചെയ്യുക, \n\nഅപ്പോൾ നിങ്ങൾക്ക് ഓട്ടോമാറ്റിക് ആയി മൂവി ലഭിക്കുന്നതാണ്.!!**"""
+
+        buttons = [
+            [
+                InlineKeyboardButton("𝗝𝗢𝗜𝗡 𝗖𝗛𝗔𝗡𝗡𝗘𝗟", url=invite_link)
+            ]
+        ]
+        
+        if file_id is False:
+            buttons.pop()
+
+        if not is_cb:
+            sh = await update.reply(
+                text=text,
+                quote=True,
+                reply_markup=InlineKeyboardMarkup(buttons),
+                parse_mode=enums.ParseMode.MARKDOWN,
+            )
+            check = await check_loop_sub(bot, update)
+            if check:
+                await sh.delete()
+                return True                               
+            else:
+                return False
+        return False
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        fix_ = await ForceSub(bot, update, file_id)
+        return fix_
+
+    except Exception as err:
+        print(f"Something Went Wrong! Unable to do Force Subscribe.\nError: {err}")
+        await update.reply(
+            text="Something went Wrong.",
+            parse_mode=enums.ParseMode.MARKDOWN,
+            disable_web_page_preview=True
+        )
+        return False
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        fix_ = await ForceSub(bot, update, file_id)
+        return fix_
+
+    except Exception as err:
+        print(f"Something Went Wrong! Unable to do Force Subscribe.\nError: {err}")
+        await update.reply(
+            text="Something went Wrong.",
+            parse_mode=enums.ParseMode.MARKDOWN,
+            disable_web_page_preview=True
+        )
+        return False
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        fix_ = await ForceSub(bot, update, file_id)
+        return fix_
+
+    except Exception as err:
+        print(f"Something Went Wrong! Unable to do Force Subscribe.\nError: {err}")
+        await update.reply(
+            text="Something went Wrong.",
+            parse_mode=enums.ParseMode.MARKDOWN,
+            disable_web_page_preview=True
+        )
+        return False
+    
+
+def set_global_invite(url: str):
+    global INVITE_LINK
+    INVITE_LINK = url
+
+async def send_file(client, query, ident, file_id):
+    files_ = await get_file_details(file_id)
+    if not files_:
+        logger.info(f"not file id {file_id}")
+        return
+    files = files_[0]
+    title = files.file_name
+    size = get_size(files.file_size)
+    f_caption = files.file_name
+    if CUSTOM_FILE_CAPTION:
+        try:
+            f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
+                                                   file_size='' if size is None else size,
+                                                   file_caption='' if f_caption is None else f_caption)
+        except Exception as e:
+            logger.exception(e)
+            f_caption = f_caption
+    if f_caption is None:
+        f_caption = f"{title}"
+    ok = await client.send_cached_media(
+        chat_id=query.from_user.id,
+        file_id=file_id,
+        caption=f_caption,
+        protect_content=True if ident == 'checksubp' else False
+    )
