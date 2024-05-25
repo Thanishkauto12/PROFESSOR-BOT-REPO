fdfebf10c03f879dbd099bf383f3887e26386271
Eva Maria Auto <88765029+Thanishkauto12@users.noreply.github.com>
2024-05-25 16:28:51 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -101,7 +101,7 @@ async def next_page(bot, query):
                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'files#{nxreq}#{file.file_id}')] for file in files ]
     btn.insert(0, [
         InlineKeyboardButton(f'ɪɴꜰᴏ', 'reqinfo'),
-        InlineKeyboardButton(f'ᴍᴏᴠɪᴇ', 'minfo'),
+        InlineKeyboardButton(f'ᴍᴏᴠɪᴇ', MINFO),
         InlineKeyboardButton(f'ꜱᴇʀɪᴇꜱ', 'sinfo')
     ])
     btn.insert(0, [
@@ -230,7 +230,7 @@ async def auto_filter(client, msg, spoll=False):
                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'{pre}#{req}#{file.file_id}')] for file in files ]
     btn.insert(0, [
         InlineKeyboardButton(f'ɪɴꜰᴏ', 'reqinfo'),
-        InlineKeyboardButton(f'ᴍᴏᴠɪᴇ', 'minfo'),
+        InlineKeyboardButton(f'ᴍᴏᴠɪᴇ', MINFO),
         InlineKeyboardButton(f'ꜱᴇʀɪᴇꜱ', 'sinfo')
     ])
     btn.insert(0, [
